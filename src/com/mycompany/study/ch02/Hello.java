c4987313639b5e58cc37cb5225a0389e5e5fdf24
yg9552 <yg4583@naver.com>
2022-07-26 15:39:27 +0900

    .

---
+++ src/com/mycompany/study/ch02/Hello.java
@@ -0,0 +1,17 @@
+package com.mycompany.study.ch02;
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+//		데이터타입	변수명 = 초기값;
+//		데이터타입 변수명
+		
+		boolean abc = false;
+		boolean xyz;
+		xyz = abc;
+		
+		
+	}
+
+}
