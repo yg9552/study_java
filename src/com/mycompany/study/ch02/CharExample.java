4bb9356d5f6adb0bdb7a0b60b1ea0ff649a1c362
yg9552 <yg4583@naver.com>
2022-07-28 17:24:24 +0900

    .

--- src/com/mycompany/study/ch02/CharExample.java
+++ src/com/mycompany/study/ch02/CharExample.java
@@ -3,8 +3,18 @@ package com.mycompany.study.ch02;
 public class CharExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+//		자바는 모든 문자를 유니코드로 처리함
+//		국제 표준 규약
+		char c1 = 'A';	//문자를 저장
+		char c2 = 65;	//십진수로 저장
+		char c3 = '\u0041';	//16진수로 저장
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		
+		
 	}
 
 }
