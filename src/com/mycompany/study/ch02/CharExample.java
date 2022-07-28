32195f58160a4d560ca69ac94f1afd4099ad0f46
yg9552 <yg4583@naver.com>
2022-07-28 17:30:37 +0900

    .

--- src/com/mycompany/study/ch02/CharExample.java
+++ src/com/mycompany/study/ch02/CharExample.java
@@ -14,7 +14,19 @@ public class CharExample {
 		System.out.println(c2);
 		System.out.println(c3);
 		
+		char c4 ='가';		//문자를 저장
+		char c5 = 44032;		//십진수로 저장
+		char c6 ='\uac00';		//16진수로 저장
 		
+		System.out.println(c4);
+		System.out.println(c5);
+		System.out.println(c6);
+		
+		int uniCode = c1;
+		System.out.println(uniCode);
+		
+		int uniCode2 = c5;
+		System.out.println(uniCode2);
 	}
 
 }
