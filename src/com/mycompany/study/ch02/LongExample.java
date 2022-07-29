cd6cc808b9e70fdf4288ad86a6c7c0535ea12c53
yg9552 <yg4583@naver.com>
2022-07-29 15:58:27 +0900

    .

---
+++ src/com/mycompany/study/ch02/LongExample.java
@@ -0,0 +1,16 @@
+package com.mycompany.study.ch02;
+
+public class LongExample {
+
+	public static void main(String[] args) {
+		
+		long var1 = 10;
+		long var2 = 156468231841235L;
+//		long var3 = 1000000000;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+//		System.out.println(var3);
+	}
+
+}
