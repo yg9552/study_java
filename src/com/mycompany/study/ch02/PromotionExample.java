34721ae819dde8bdb93044e9a63caea4cbda5b26
yg9552 <yg4583@naver.com>
2022-07-29 17:12:09 +0900

    .

--- src/com/mycompany/study/ch02/PromotionExample.java
+++ src/com/mycompany/study/ch02/PromotionExample.java
@@ -3,7 +3,7 @@ package com.mycompany.study.ch02;
 public class PromotionExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
 //		작은 형이 큰 형으로 변환하는건 문제없다
 //		큰 형이 작은 형으로 변환되려면 문제가 생기거나 변환이 이루어져도 데이터 손실이난다.
