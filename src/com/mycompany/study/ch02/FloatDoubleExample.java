58834fda687c0bab1c432000931030e1feaa75ed
yg9552 <yg4583@naver.com>
2022-07-29 15:59:46 +0900

    .

---
+++ src/com/mycompany/study/ch02/FloatDoubleExample.java
@@ -0,0 +1,12 @@
+package com.mycompany.study.ch02;
+
+public class FloatDoubleExample {
+
+	public static void main(String[] args) {
+		
+		double var1 = 3.14;
+		float var2 = 3.14f;
+		float var3 = 3.14F;
+	}
+
+}
