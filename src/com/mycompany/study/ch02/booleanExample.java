c9e1fedf4f9ffcb564ead43082a9518f117a92e9
yg9552 <yg4583@naver.com>
2022-07-29 16:03:54 +0900

    .

---
+++ src/com/mycompany/study/ch02/booleanExample.java
@@ -0,0 +1,16 @@
+package com.mycompany.study.ch02;
+
+public class booleanExample {
+
+	public static void main(String[] args) {
+
+		int var1 = 10;
+		int var2 = 012;		//8진수표기법
+		int var3 = 0xa;		//16진수 표기법
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+	}
+
+}
