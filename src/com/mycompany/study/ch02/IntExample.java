ca44e246dbf34ec7e7112823f5a339b6ebd09fcd
yg9552 <yg4583@naver.com>
2022-07-29 15:53:13 +0900

    .

--- src/com/mycompany/study/ch02/IntExample.java
+++ src/com/mycompany/study/ch02/IntExample.java
@@ -5,7 +5,12 @@ public class IntExample {
 	public static void main(String[] args) {
 
 		int var1 = 10;
+		int var2 = 012;		//8진수표기법
+		int var3 = 0xa;		//16진수 표기법
+		
 		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
 	}
 
 }
