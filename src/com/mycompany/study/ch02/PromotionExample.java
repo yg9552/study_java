06ba6c41444faa541c5ec5a001ffbe8eaed51a6a
yg9552 <yg4583@naver.com>
2022-08-01 16:40:06 +0900

    .

--- src/com/mycompany/study/ch02/PromotionExample.java
+++ src/com/mycompany/study/ch02/PromotionExample.java
@@ -5,6 +5,7 @@ public class PromotionExample {
 	public static void main(String[] args) {
 	
 
+//		자동타입변환
 //		작은 형이 큰 형으로 변환하는건 문제없다
 //		큰 형이 작은 형으로 변환되려면 문제가 생기거나 변환이 이루어져도 데이터 손실이난다.
 //		byte < short < int < long < float <double
@@ -26,6 +27,27 @@ public class PromotionExample {
 		
 		double doubleValue = intValue3;
 		System.out.println("doubleValue:" + doubleValue);
+		
+		
+//		short를 int로 변환
+		short shortvalue = 10000;
+		int intvalue4 = shortvalue;
+		System.out.println(intvalue4);
+		
+//		int를 long으로 변환
+		int intvalue5 = 1000000000;
+		long longvalue = intvalue5;
+		System.out.println(longvalue);
+		
+//		long을 float으로 변환
+		long longvalue1 = 5000000000000000000L;
+		float floatvalue = longvalue1;
+		System.out.println(floatvalue);
+		
+//		float을 double로 변환
+		float floatvalue2 = 300000000000000000000000000000000000000F;
+		double doublevalue1 = floatvalue2;
+		System.out.println(doublevalue1);
 	}
 
 }
