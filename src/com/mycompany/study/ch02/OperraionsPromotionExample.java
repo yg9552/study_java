34e0533ad3c40f6c9359246c4a0ff8eac30065a0
yg9552 <yg4583@naver.com>
2022-08-02 14:59:19 +0900

    .

--- src/com/mycompany/study/ch02/OperraionsPromotionExample.java
+++ src/com/mycompany/study/ch02/OperraionsPromotionExample.java
@@ -10,29 +10,29 @@ public class OperraionsPromotionExample {
 		int intValue = byteValue + byteValue2;
 		System.out.println(intValue);
 		
-		short shortValue = 384;
-		short shortValue2 = 384;
+		short shortValue = 24;
+		short shortValue2 = 24;
 //		short shortValue3 = shortValue + shortValue2;
 		int shortValue3 = shortValue + shortValue2;
 		System.out.println(shortValue3);
 		
-		int intValue1 = 24;
-		int intValue2 = 24;
+		int intValue1 = 48;
+		int intValue2 = 48;
 		int intValue3 = intValue1 + intValue2;
 		System.out.println(intValue3);
 		
-		long longValue = 48;
-		long longValue2 = 48;
+		long longValue = 96;
+		long longValue2 = 96;
 		long longValue3 = longValue + longValue2;
 		System.out.println(longValue3);
 		
-		float floatValue = 96;
-		float floatValue2 = 96;
+		float floatValue = 192;
+		float floatValue2 = 192;
 		float floatValue3 = floatValue + floatValue2;
 		System.out.println(floatValue3);
 		
-		double doubleValue = 192;
-		double doubleValue2 = 192;
+		double doubleValue = 384;
+		double doubleValue2 = 384;
 		double doubleValue3 = doubleValue + doubleValue2;
 		System.out.println(doubleValue3);
 		
