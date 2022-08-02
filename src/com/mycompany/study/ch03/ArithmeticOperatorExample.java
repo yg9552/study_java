4979ccc5d21c7ec9f8581e376199deab5c2a7f8f
yg9552 <yg4583@naver.com>
2022-08-02 16:05:36 +0900

    .

---
+++ src/com/mycompany/study/ch03/ArithmeticOperatorExample.java
@@ -0,0 +1,14 @@
+package com.mycompany.study.ch03;
+
+public class ArithmeticOperatorExample {
+
+	public static void main(String[] args) {
+
+		int v1 = 5;
+		int v2 = 2;
+		
+		int result = v1 + v2;
+		System.out.println(result);
+	}
+
+}
