eb481aaedf950b486abdb6fe381d27ddc7949a6d
yg9552 <yg4583@naver.com>
2022-08-02 15:06:18 +0900

    .

--- src/com/mycompany/study/ch03/SignOperatorExample.java
+++ src/com/mycompany/study/ch03/SignOperatorExample.java
@@ -4,7 +4,11 @@ public class SignOperatorExample {
 
 	public static void main(String[] args) {
 
-		
+		int x =100;
+		int result1 = +x;
+		int result2 = -x;
+		System.out.println(result1);
+		System.out.println(result2);
 	}
 
 }
