0eb57cb0e2f9701197cd5f86c1b8128afa148558
yg9552 <yg4583@naver.com>
2022-08-02 16:03:54 +0900

    .

--- src/com/mycompany/study/ch03/DenyLogicOperatorExample.java
+++ src/com/mycompany/study/ch03/DenyLogicOperatorExample.java
@@ -4,7 +4,29 @@ public class DenyLogicOperatorExample {
 
 	public static void main(String[] args) {
 
+		boolean p = true;
 		
+		System.out.println(p);
+		
+//		논리부정연산자 : !
+		p = !p;
+		System.out.println(p);
+		
+		p= !p;
+		System.out.println(p);
+		p= !p;
+		System.out.println(p);
+		p= !p;
+		System.out.println(p);
+		
+		int j = 3;
+		
+		if (!(j==3)) {
+			System.out.println("3이 아님");
+		} else {
+				System.out.println("3");
+			}
+		}
 	}
 
-}
+
