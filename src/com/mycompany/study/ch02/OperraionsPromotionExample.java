fd4b46a0247bf7576527664d32b28ff5c6def41d
yg9552 <yg4583@naver.com>
2022-08-02 14:02:49 +0900

    .

---
+++ src/com/mycompany/study/ch02/OperraionsPromotionExample.java
@@ -0,0 +1,16 @@
+package com.mycompany.study.ch02;
+
+public class OperraionsPromotionExample {
+
+	public static void main(String[] args) {
+
+		byte byteValue = 12;
+		byte byteValue2 = 12;
+		byte byteValue3 = byteValue + byteValue2;
+		System.out.println(byteValue3);
+		
+//		byte byteValue3 = (byte) (byteValue + byteValue2);
+		
+	}
+
+}
