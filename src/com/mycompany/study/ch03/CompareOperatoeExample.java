0e083d6f6dcad005491661691fdc709245ba9ddd
yg9552 <yg4583@naver.com>
2022-08-03 17:20:40 +0900

    .

--- src/com/mycompany/study/ch03/CompareOperatoeExample.java
+++ src/com/mycompany/study/ch03/CompareOperatoeExample.java
@@ -10,6 +10,26 @@ public class CompareOperatoeExample {
 		boolean result1 = num == num2 ;
 		System.out.println(result1);
 		
+		boolean result2 = num != num2 ;
+		System.out.println(result2);
+
+		boolean result3 = num > num2 ;
+		System.out.println(result3);
+		
+		boolean result4 = num >= num2 ;
+		System.out.println(result4);
+		
+		boolean result5 = num < num2 ;
+		System.out.println(result5);
+
+		boolean result6 = num <= num2 ;
+		System.out.println(result6);
+		
+		boolean result7 = result5 == result6 ;
+		System.out.println(result7);
+		
+		
+		
 	}
 
 }
