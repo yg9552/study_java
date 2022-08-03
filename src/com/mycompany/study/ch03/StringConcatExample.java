ac3685bd00223e4c7f1c3124257ebeb9ff8cdf9b
yg9552 <yg4583@naver.com>
2022-08-03 17:14:06 +0900

    .

---
+++ src/com/mycompany/study/ch03/StringConcatExample.java
@@ -0,0 +1,16 @@
+package com.mycompany.study.ch03;
+
+public class StringConcatExample {
+
+	public static void main(String[] args) {
+
+		String StringValue = "jdk";
+		String StringValue2 = "1.11";
+		
+		System.out.println("우리가 사용하는 " + StringValue + StringValue2 + "입니다");
+		
+		
+		
+	}
+
+}
