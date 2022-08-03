ce66eb475a9f0708c650a81353e33dd9c0f389c7
yg9552 <yg4583@naver.com>
2022-08-03 17:17:13 +0900

    .

---
+++ src/com/mycompany/study/ch03/CompareOperatoeExample.java
@@ -0,0 +1,15 @@
+package com.mycompany.study.ch03;
+
+public class CompareOperatoeExample {
+
+	public static void main(String[] args) {
+
+		int num = 10;
+		int num2 = 30;
+		
+		boolean result1 = num == num2 ;
+		System.out.println(result1);
+		
+	}
+
+}
