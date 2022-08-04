ca0fdeff7e16f53dae2a5b4d917eb01503f9add1
yg9552 <yg4583@naver.com>
2022-08-04 17:08:18 +0900

    .

---
+++ src/com/mycompany/study/ch03/ConditionalOperatorExample.java
@@ -0,0 +1,22 @@
+package com.mycompany.study.ch03;
+
+public class ConditionalOperatorExample {
+
+	public static void main(String[] args) {
+
+		int score = 85 ;
+		
+		char grade = score > 90 ? 'A' : 'B';
+		
+		System.out.println("grade: " + grade);
+		
+		System.out.println("----------------------------");
+		
+		if (grade > 90) {
+			System.out.println("grade: A");
+		} else {
+			System.out.println("grade: B");
+		}
+	}
+
+}
--- src/com/mycompany/study/ch03/LogicalOperatorExample.java
+++ src/com/mycompany/study/ch03/LogicalOperatorExample.java
@@ -3,8 +3,61 @@ package com.mycompany.study.ch03;
 public class LogicalOperatorExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+//		&& = & : and : 논리곱 : 연산 참 참 참, 나머지 폴스
+//		|| = | : or : 논리합 : 연산 참 폴스 참, 폴스 참 참, 폴스 폴스 폴스
+		
+		int a = 65;
+		
+		boolean result1 = (a>= 65);
+		System.out.println(result1);
+		
+		boolean result2 = (a<= 90);
+		System.out.println(result2);
+		
+		if (a >= 65 & a <= 90) {
+			System.out.println("65보다 크거나 같고 90보다 작거나 같습니다");
+		}
+		if (a >= 65 && a <= 90) {
+			System.out.println("65보다 크거나 같고 90보다 작거나 같습니다");
+		}
+		
+		System.out.println("------------------------------------------------");
+		
+		if (a >=65 | a <= 90) {
+			System.out.println("둘다 참");
+		}
+		
+		if (a >= 66 | a <= 90) {
+			System.out.println("하나만 참");
+		}
+		if (a >= 66 | a <=50) {
+			System.out.println("둘다 거짓이라 안보임");
+		}
+		if (a >=65 || a <= 90) {
+			System.out.println("둘다 참");
+		}
+		
+		if (a >= 66 || a <= 90) {
+			System.out.println("하나만 참");
+		}
+		if (a >= 66 || a <=50) {
+			System.out.println("둘다 거짓이라 안보임");
+		}
+		
+		System.out.println("------------------------------------------------");
+		
+		if (a >=65 ^ a <= 90) {
+			System.out.println("둘다 참이라 안보임");
+		}
+		if (a < 65 ^ a >=90) {
+			System.out.println("둘다 거짓이라 안보임");
+		}
+		if (a >=65 ^ a <= 50) {
+			System.out.println("하나만 참");
+		}
+		
+		
 	}
 
 }
