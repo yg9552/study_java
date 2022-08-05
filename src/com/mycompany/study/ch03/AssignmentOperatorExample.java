ce4748915ceaf22df2ccf12bd54519ff097b976e
yg9552 <yg4583@naver.com>
2022-08-05 12:27:57 +0900

    .

---
+++ src/com/mycompany/study/ch03/AssignmentOperatorExample.java
@@ -0,0 +1,26 @@
+package com.mycompany.study.ch03;
+
+public class AssignmentOperatorExample {
+
+	public static void main(String[] args) {
+
+//		+=
+		
+		int result = 0;
+		
+		result += 10;
+//		result = result +10;
+		System.out.println("result: " + result);
+		
+//		-=
+		
+		result -= 10;
+		System.out.println("result: " + result);
+		
+//		*=
+		result *= 10;
+		System.out.println("result: " + result);
+		
+	}
+
+}
