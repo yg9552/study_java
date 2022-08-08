485dd4893ce175d7e55e7f13a0dc93ccd6781b0e
yg9552 <yg4583@naver.com>
2022-08-08 17:00:07 +0900

    .

---
+++ src/com/mycompany/study/ch04/IfExample.java
@@ -0,0 +1,14 @@
+package com.mycompany.study.ch04;
+
+public class IfExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int score = 80 ;
+		if (score >90) {
+			System.out.println("A");
+		}
+	}
+
+}
