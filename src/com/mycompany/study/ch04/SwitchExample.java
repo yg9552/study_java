acf85e060b30b21ae10a0ed03f6859248238d5b5
yg9552 <yg4583@naver.com>
2022-08-09 17:18:53 +0900

    .

---
+++ src/com/mycompany/study/ch04/SwitchExample.java
@@ -0,0 +1,42 @@
+package com.mycompany.study.ch04;
+
+public class SwitchExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 2; //1 , 5
+		
+		switch(num){
+			case 1:
+				System.out.println("1번이 나왔습니다");
+				break;
+			case 2:
+				System.out.println("2번이 나왔습니다");
+				break;
+			default:
+				System.out.println("1도 2도 아닌 정수입니다");
+				break;
+		}
+		
+		String position = "사장";
+		
+		switch(position) {
+			case "사원":
+				System.out.println("연봉은 5,000 입니다.");
+				break;
+			case "대리":
+				System.out.println("연봉은 5,100 입니다.");
+				break;
+			case "부장":
+				System.out.println("연봉은 5,200 입니다.");
+				break;
+			case "사장":
+				System.out.println("연봉은 5,500 입니다.");
+				break;
+			default:
+				System.out.println("회장입니다.");
+				break;
+		}
+	}
+
+}
