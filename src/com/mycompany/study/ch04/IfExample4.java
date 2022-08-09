806224e19807f41deecd80d04b0cb6a1ef1e5be8
yg9552 <yg4583@naver.com>
2022-08-09 15:58:23 +0900

    .

--- src/com/mycompany/study/ch04/IfExample4.java
+++ src/com/mycompany/study/ch04/IfExample4.java
@@ -7,18 +7,23 @@ public class IfExample4 {
 //		int 타입의 변수를 2개 선언하고
 //		두 정수가 주어졌을때 사분면의 어디에 해당하는지
 		
-		int x = 23;
-		int y = 4;
+		int x = 0;
+		int y = 10;
 		
-		if (x < 0 & y > 0) {
+		if (x < 0 && y > 0) {
 			System.out.println("1사분면");
-		} else if (x > 0 & y > 0){
+		} else if (x > 0 && y > 0){
 			System.out.println("2사분면");
-		} else if (x < 0 & y < 0) {
+		} else if (x < 0 && y < 0) {
 			System.out.println("3사분면");
-		} else if (x > 0 & y < 0) {
+		} else if (x > 0 && y < 0) {
 			System.out.println("4사분면");
-		} else {
+		} else if (x == 0) {
+			System.out.println("x축 위에 있습니다");
+		} else if (y == 0) {
+			System.out.println("y축 위에 있습니다");
+		}
+		else {
 			System.out.println("둘다0입니다");
 		}
 	}
---
+++ src/com/mycompany/study/ch04/IfExample5.java
@@ -0,0 +1,10 @@
+package com.mycompany.study.ch04;
+
+public class IfExample5 {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}
