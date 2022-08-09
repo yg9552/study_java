e021f84a4a14a625617597b529880fa50dcf2805
yg9552 <yg4583@naver.com>
2022-08-09 11:08:23 +0900

    .

--- src/com/mycompany/study/ch03/ConditionalOperatorExample.java
+++ src/com/mycompany/study/ch03/ConditionalOperatorExample.java
@@ -4,19 +4,19 @@ public class ConditionalOperatorExample {
 
 	public static void main(String[] args) {
 
-//		int score = 85 ;
-//		
-//		char grade = score > 90 ? 'A' : 'B';
-//		
-//		System.out.println("grade: " + grade);
-//		
-//		System.out.println("----------------------------");
-//		
-//		if (grade > 90) {
-//			System.out.println("grade: A");
-//		} else {
-//			System.out.println("grade: B");
-//		}
+		int score = 85 ;
+		
+		char grade = score > 90 ? 'A' : 'B';
+		
+		System.out.println("grade: " + grade);
+		
+		System.out.println("----------------------------");
+		
+		if (grade > 90) {
+			System.out.println("grade: A");
+		} else {
+			System.out.println("grade: B");
+		}
 		
 		
 	}
