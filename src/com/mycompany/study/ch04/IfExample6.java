3fce77fcc457f33c8392df3c1ad885f5fa66e537
yg9552 <yg4583@naver.com>
2022-08-09 16:53:48 +0900

    .

--- src/com/mycompany/study/ch04/IfExample6.java
+++ src/com/mycompany/study/ch04/IfExample6.java
@@ -8,24 +8,27 @@ public class IfExample6 {
 //		int타입의 변수 3개를 선언하고
 //		최소값 최대값을 구하는?
 		
-		int a = 6;
-		int b = 23;
-		int c = 3;
+		int a = 1;
+		int b = 3;
+		int c = 8;
 		
-		if (a < b && a < c) {
+		if (a != b && a !=c && b != c) {
+			if (a < b && a < c) {
 			System.out.println("a는 최소값입니다");
-		} else if (b < a && b < c){
+			} else if (b < a && b < c){
 			System.out.println("b는 최소값입니다");
-		} else {
+			} else {
 			System.out.println("c는 최소값입니다");
-		}
-		if (a > b && a > c) {
+			}
+			if (a > b && a > c) {
 			System.out.println("a는 최대값입니다");
-		} else if (b >a && b > c) {
+			} else if (b >a && b > c) {
 			System.out.println("b는 최대값입니다");
-		} else {
+			} else {
 			System.out.println("c는 최대값입니다");
+			}
+		} else {
+			System.out.println("a ,b, c 셋중 둘은 같습니다");
 		}
-		
 	}
 }
