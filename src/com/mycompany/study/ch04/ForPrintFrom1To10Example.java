6dd77e7e6f14ca3396239cd0273316420d1dd0d4
yg955 <yg955@LAPTOP-RVSAR8PS>
2022-08-10 14:53:43 +0900

    .

--- src/com/mycompany/study/ch04/ForPrintFrom1To10Example.java
+++ src/com/mycompany/study/ch04/ForPrintFrom1To10Example.java
@@ -1,11 +1,26 @@
 package com.mycompany.study.ch04;
 
+import java.util.Iterator;
+
 public class ForPrintFrom1To10Example {
 
 	public static void main(String[] args) {
 	
+//		int sum = 0;
+//		for(int i = 1; i <= 10; i++) {
+//			System.out.println(i);
+//			sum += i;	//sum = sum + i;
+//		}
+//		System.out.println("sum : " + sum);
+		
+//		1 부터 10 까지 더하는 프로그램 작성
 		int sum = 0;
 		
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+			sum += i;
+		}
+		System.out.println("sum: " + sum);
 	}
 
 }
