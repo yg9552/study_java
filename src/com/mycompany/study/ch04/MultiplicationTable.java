72d2db0e0d841cb62fdf15edbb3b0afe9454c053
yg955 <yg955@LAPTOP-RVSAR8PS>
2022-08-10 15:12:17 +0900

    .

--- src/com/mycompany/study/ch04/MultiplicationTable.java
+++ src/com/mycompany/study/ch04/MultiplicationTable.java
@@ -6,8 +6,16 @@ public class MultiplicationTable {
 
 //		구구단 2단 출력
 		
-		for (int i = 1; i <= 9; i++) {
-			System.out.println("2 *" + i + " = " + 2*i);
+//		for (int i = 1; i <= 9; i++) {
+//			System.out.println("2 * " + i + " = " + 2*i);
+//		}
+//		
+//		2단 부터 9단 까지 출력
+		
+		for (int i = 2; i <= 9; i++) {
+			for (int j = 1; j <= 9; j++) {
+				System.out.println("i * j = " + i * j);
+			}
 		}
 	}
 
