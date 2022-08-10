6aa0fd82752a1f11089324361e9abb223aea5a66
yg955 <yg955@LAPTOP-RVSAR8PS>
2022-08-10 14:56:05 +0900

    .

---
+++ src/com/mycompany/study/ch04/MultiplicationTable.java
@@ -0,0 +1,14 @@
+package com.mycompany.study.ch04;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+
+//		구구단 2단 출력
+		
+		for (int i = 1; i <= 9; i++) {
+			System.out.println("2 *" + i + " = " + 2*i);
+		}
+	}
+
+}
