90ed6b176b12be74bf4919f858907611d70140f9
yg955 <yg955@LAPTOP-RVSAR8PS>
2022-08-10 17:08:52 +0900

    .

--- src/com/mycompany/study/ch04/RandomSum.java
+++ src/com/mycompany/study/ch04/RandomSum.java
@@ -6,11 +6,33 @@ public class RandomSum {
 
 //		1~100사이의 랜덤 정수 2개를 발생시켜
 //		두 수 사이의 합을 구하라
-		
 		double min = 1;
 		double max = 100;
-		int random = (int) (Math.random() * (max + min) + min);
-		System.out.println(random);
+		int random1 = (int) (Math.random() * (max + min));
+		int random2 = (int) (Math.random() * (max + min));
+		System.out.println(random1);
+		System.out.println(random2);
+		System.out.println(random1+ " + " + random2 + " = " + (random1 + random2));
+		if (random1<random2) {
+			for (int i = random1; i<random2; i++) {
+				for (int j =random2; j >random2; j--) {
+					System.out.println(random1 + random2);
+				}
+		}
+		
+		}
+		
+		
+		
+//		두 수의 합
+//		double min = 1;
+//		double max = 100;
+//		int random1 = (int) (Math.random() * (max + min));
+//		int random2 = (int) (Math.random() * (max + min));
+//		System.out.println(random1);
+//		System.out.println(random2);
+//		System.out.println(random1+ " + " + random2 + " = " + (random1 + random2));
+//		}
 		
 		
 		
@@ -20,8 +42,9 @@ public class RandomSum {
 //		double max = 45.0;
 //		
 //		for (int i = 1; i <= 6; i++) {
-//			int random = (int) (Math.random() * (max + min) + min);
+//			int random = (int) (Math.random() * (max + min));
 //			System.out.println(i + " = " + random);
+//			
 //		}
 		
 	}
