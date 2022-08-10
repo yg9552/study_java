41897f6f10bd76bb7b57903d1f35a25f9e7f7991
yg955 <yg955@LAPTOP-RVSAR8PS>
2022-08-10 15:23:12 +0900

    .

--- src/com/mycompany/study/ch04/MultiplicationTable.java
+++ src/com/mycompany/study/ch04/MultiplicationTable.java
@@ -14,9 +14,14 @@ public class MultiplicationTable {
 		
 		for (int i = 2; i <= 9; i++) {
 			for (int j = 1; j <= 9; j++) {
-				System.out.println("i * j = " + i * j);
+				System.out.println(i + " * " +  j + " = " + i * j);
+				if (j == 9) {
+					System.out.println("------------------");
+				}
 			}
+			
 		}
+		
 	}
 
 }
