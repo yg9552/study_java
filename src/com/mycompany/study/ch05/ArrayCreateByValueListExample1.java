77cd83ec576eacfb4da752be7a12335e3db57d9b
yg9552 <yg4583@naver.com>
2022-08-17 16:18:19 +0900

    .

---
+++ src/com/mycompany/study/ch05/ArrayCreateByValueListExample1.java
@@ -0,0 +1,16 @@
+package com.mycompany.study.ch05;
+
+public class ArrayCreateByValueListExample1 {
+
+	public static void main(String[] args) {
+		int [] scoreEnglish;
+		int [] scoreMath;
+		
+		int[]scoreChemistry = {88,99,45,86,34};
+		
+		
+		String []name = {"장원영", "장동건", "고소영"};
+		
+	}
+
+}
