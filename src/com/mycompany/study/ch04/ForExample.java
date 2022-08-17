e1b84dc20f038b6a4c9be90189da0a4a701953e6
yg9552 <yg4583@naver.com>
2022-08-17 15:09:33 +0900

    .

--- src/com/mycompany/study/ch04/ForExample.java
+++ src/com/mycompany/study/ch04/ForExample.java
@@ -10,6 +10,14 @@ public class ForExample {
 			System.out.println(string);
 		}
 		
+		System.out.println("------------------");
+		
+		for (int i = 1; i<=5; i++) {
+			for (int j = 1; j<=10; j++) {
+				System.out.print("*");
+			}System.out.println();
+		}
+		
 		
 		System.out.println("---------------------");
 		
@@ -37,6 +45,22 @@ public class ForExample {
 				System.out.print(i);
 			}
 		} System.out.println(0);
+		
+		System.out.println("---------------------");
+		
+		for (int i = 1; i <=5; i++) {
+			for (int j = 1; j <= 10; j++) {
+				if(i > 1 && i < 5) {
+					if (j > 1 && j < 10) {
+						System.out.print(" ");
+					} else {
+						System.out.print("*");
+					}
+				} else {
+					System.out.print("*");
+				}
+			}System.out.println();
+		}
 		
 	}
 }
