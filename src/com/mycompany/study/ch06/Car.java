af72be8436abc5321d3b282a14fe0cc85a5f8424
yg9552 <yg4583@naver.com>
2022-08-22 15:58:10 +0900

    .

---
+++ src/com/mycompany/study/ch06/Car.java
@@ -0,0 +1,16 @@
+package com.mycompany.study.ch06;
+
+public class Car {
+
+	//field : 변수
+	int a = 1;
+	String name ="G70";
+	String[] name2 = {"G80","G90"};
+	
+	
+	
+	//method : 함수
+	
+	
+	
+}
---
+++ src/com/mycompany/study/ch06/CarExample.java
@@ -0,0 +1,17 @@
+package com.mycompany.study.ch06;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+
+		Car abc = new Car();
+		
+		int a = 1;
+		
+		System.out.println("abc.name : " + abc.name);
+		System.out.println("abc.name2[0] : " + abc.name2[0]);
+		System.out.println("abc.name2[1] : " + abc.name2[1]);
+		
+	}
+
+}
