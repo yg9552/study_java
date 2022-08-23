726f39e22a22b1ae09b783088144040611e40177
yg9552 <yg4583@naver.com>
2022-08-23 17:04:46 +0900

    .

--- src/com/mycompany/study/ch06/CarSecond.java
+++ src/com/mycompany/study/ch06/CarSecond.java
@@ -2,21 +2,61 @@ package com.mycompany.study.ch06;
 
 public class CarSecond {
 	
-	String color = "black";
-	int cc = 1500;
-	int tire = 19;
-	String name = "G70";
+//	String color = "black";
+//	int cc = 1500;
+//	int tire = 19;
+//	String name = "G70";
+//	
+//	CarSecond(String str) {
+//		color = str;
+//		name = str;
+//	}
+//	CarSecond(int num) {
+//		cc = num;
+//		tire = num;
+//	}
+//	
+//	CarSecond(){
+//		
+//	}
 	
-	CarSecond(String str) {
-		color = str;
-		name = str;
-	}
-	CarSecond(int num) {
-		cc = num;
-		tire = num;
-	}
+	String name = "아반떼";
+	int cc;
+	String color;
 	
-	CarSecond(){
+//	CarSecond(String str) {
+//		name = str;
+//	}
+//	
+//	CarSecond (String str, int num) {
+//		name = str;
+//		cc = num;
+//	}
+//	CarSecond (String str, int num, String str2) {
+//		name = str;
+//		cc = num;
+//		color = str2;
+//	}
+	
+//	기본생성자
+	CarSecond () {
 		
 	}
+	
+	CarSecond(String name) {
+		this.name = name;
+	}
+	CarSecond(int cc) {
+		this.cc = cc;
+	}
+	CarSecond(String name, int cc) {
+		this.name = name;
+		this.cc = cc;
+	}
+	CarSecond(String name, int cc, String color) {
+		this.name = name;
+		this.cc = cc;
+		this.color = color;
+	}
+	
 }
--- src/com/mycompany/study/ch06/CarSecondExample.java
+++ src/com/mycompany/study/ch06/CarSecondExample.java
@@ -4,24 +4,37 @@ public class CarSecondExample {
 
 	public static void main(String[] args) {
 
-		CarSecond carsecond = new CarSecond();
-		System.out.println("cc : " + carsecond.cc);
-		System.out.println("color : " + carsecond.color);
-		System.out.println("타이어 사이즈 : " + carsecond.tire + "인치");
+//		CarSecond carsecond = new CarSecond();
+//		System.out.println("cc : " + carsecond.cc);
+//		System.out.println("color : " + carsecond.color);
+//		System.out.println("타이어 사이즈 : " + carsecond.tire + "인치");
+//		
+//		
+//		CarSecond carsecond2 = new CarSecond("white");
+//		System.out.println("color : " + carsecond2.color);
+//		
+//		
+//		CarSecond carsecond3 = new CarSecond(2000);
+//		System.out.println("cc : " + carsecond3.cc);
+//		
+//		CarSecond carsecond4 = new CarSecond("G80");
+//		System.out.println("name : " + carsecond4.name);
+//		
+//		CarSecond carsecond5 = new CarSecond(21);
+//		System.out.println("타이어 사이즈 : " + carsecond5.tire + "인치");
+
+		CarSecond carSecond = new CarSecond();
+		System.out.println("모델명 : " + carSecond.name);
 		
+		CarSecond carSecond1 = new CarSecond("그랜저",3000,"White");
+		System.out.println("모델명 : " + carSecond1.name + " | CC : " + carSecond1.cc + " | 색상 : " + carSecond1.color);
+	
+		CarSecond carSecond2 = new CarSecond("쏘나타", 1600);
+		System.out.println("모델명 : " + carSecond2.name + " | CC : " + carSecond2.cc);
+
+		CarSecond carSecond3 = new CarSecond("캐스퍼");
+		System.out.println("모델명 : " + carSecond3.name);
 		
-		CarSecond carsecond2 = new CarSecond("white");
-		System.out.println("color : " + carsecond2.color);
-		
-		
-		CarSecond carsecond3 = new CarSecond(2000);
-		System.out.println("cc : " + carsecond3.cc);
-		
-		CarSecond carsecond4 = new CarSecond("G80");
-		System.out.println("name : " + carsecond4.name);
-		
-		CarSecond carsecond5 = new CarSecond(21);
-		System.out.println("타이어 사이즈 : " + carsecond5.tire + "인치");
 	}
 
 }
