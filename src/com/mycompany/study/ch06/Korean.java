b71fa09773c69166cdfb87e51cb9e0d9ab674e58
yg9552 <yg4583@naver.com>
2022-08-23 17:18:21 +0900

    .

---
+++ src/com/mycompany/study/ch06/Korean.java
@@ -0,0 +1,22 @@
+package com.mycompany.study.ch06;
+
+public class Korean {
+
+	String name;
+	String ssn;
+	int age;
+	
+	Korean() {
+		
+	}
+	
+	Korean(String name, String ssn) {
+		this.name = name;
+		this.ssn = ssn;
+	}
+	Korean(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+}
---
+++ src/com/mycompany/study/ch06/KoreanExample.java
@@ -0,0 +1,18 @@
+package com.mycompany.study.ch06;
+
+public class KoreanExample {
+
+	public static void main(String[] args) {
+
+		Korean k1 = new Korean("장원영", "011255-1234567");
+		System.out.println("k1.name : " + k1.name);
+		System.out.println("k1.ssn : " + k1.ssn);
+
+		Korean k2 = new Korean("장원영", 45);
+		System.out.println("k2.name : " + k2.name);
+		System.out.println("k2.age : " + k2.age);
+		
+		
+	}
+
+}
