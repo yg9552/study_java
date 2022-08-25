a777d323c496d8ecc0204a99d2110c5a0cc7c554
yg9552 <yg4583@naver.com>
2022-08-25 15:52:45 +0900

    .

--- src/com/mycompany/study/ch07/first/DmbCellPhoneExample.java
+++ src/com/mycompany/study/ch07/first/DmbCellPhoneExample.java
@@ -15,7 +15,7 @@ public class DmbCellPhoneExample {
 		dmbCellPhone.turnOffDmb();
 		
 		// 생성자를 이용하여 자식객체를 생성
-		DmbCellPhone dmbCellPhone2 = new DmbCellPhone("IPhone 12 Pro", "Gold", 10);
+		DmbCellPhone dmbCellPhone2 = new DmbCellPhone("IPhone 12 Pro", "퍼시픽블루", 10);
 		
 //		dmbCellPhone2 객체를 이용하여 자식 클래스의 매서드 출력
 		dmbCellPhone2.turnOnDmb();
