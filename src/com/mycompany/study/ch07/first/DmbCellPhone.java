a4b653cc232f9bc137d2157d6d7796ded4898b4a
yg9552 <yg4583@naver.com>
2022-08-25 15:47:46 +0900

    .

--- src/com/mycompany/study/ch07/first/DmbCellPhone.java
+++ src/com/mycompany/study/ch07/first/DmbCellPhone.java
@@ -20,7 +20,7 @@ public class DmbCellPhone extends CellPhone{
 	void turnOnDmb() {
 		System.out.println("채널 " + channel + "번 DMB방송 수신을 시작합니다.");
 	}
-	void changeChannelDmb() {
+	void changeChannelDmb(int channel) {
 		this.channel = channel;
 		System.out.println("채널 " + channel + "번으로 바꿨습니다.");
 	}
---
+++ src/com/mycompany/study/ch07/first/DmbCellPhoneExample.java
@@ -0,0 +1,34 @@
+package com.mycompany.study.ch07.first;
+
+public class DmbCellPhoneExample {
+
+	public static void main(String[] args) {
+		
+		// 부모 객체를 생성하고 powerOn 실행
+		CellPhone cellPhone = new CellPhone();
+		
+		cellPhone.powerOn();
+		
+		// 자식 객체를 생성 하고 DMB 끄기
+		DmbCellPhone dmbCellPhone = new DmbCellPhone();
+		
+		dmbCellPhone.turnOffDmb();
+		
+		// 생성자를 이용하여 자식객체를 생성
+		DmbCellPhone dmbCellPhone2 = new DmbCellPhone("IPhone 12 Pro", "Gold", 10);
+		
+//		dmbCellPhone2 객체를 이용하여 자식 클래스의 매서드 출력
+		dmbCellPhone2.turnOnDmb();
+		dmbCellPhone2.changeChannelDmb(1);
+		dmbCellPhone2.turnOffDmb();
+		
+//		dmbCellPhone2 객체를 이용하여 부모 클래스의 매서드 출력
+		dmbCellPhone2.powerOn();
+		dmbCellPhone2.bell();
+		dmbCellPhone2.sendVoice("ㄴㅁㅇㄹㄴㅇㅁㄹㅈㄷㄱ");
+		dmbCellPhone2.receiveVoice("ㄴㅁㅇㄹㄴㅇㅁㄹㅈㄷㄱ");
+		dmbCellPhone2.hangUp();
+		dmbCellPhone2.powerOff();
+	}
+
+}
