08136022a850e0ed63ce075a32cabed3c34070b1
yg9552 <yg4583@naver.com>
2022-08-25 16:52:17 +0900

    .

---
+++ src/com/mycompany/study/ch07/second/People.java
@@ -0,0 +1,16 @@
+package com.mycompany.study.ch07.second;
+
+public class People {
+
+	public String name;
+	public String ssn;
+	
+	//부모에 생성자가 존재한다
+	public People(String name, String ssn){
+		this.name = name;
+		this.ssn = ssn;
+	}
+	public People() {
+		
+	}
+}
---
+++ src/com/mycompany/study/ch07/second/Student.java
@@ -0,0 +1,20 @@
+package com.mycompany.study.ch07.second;
+
+public class Student extends People{
+
+	public int studentNo;
+	
+	public Student(String name, String ssn, int studentNo){
+		super(name, ssn);
+		this.studentNo = studentNo;
+	}
+	
+	public Student(String name, String ssn) {
+		super(name, ssn);
+	}
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+}
