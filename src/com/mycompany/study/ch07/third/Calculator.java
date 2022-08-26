b7337b7284a14d8308cf8964078210ff1293b981
yg9552 <yg4583@naver.com>
2022-08-26 16:23:10 +0900

    .

---
+++ src/com/mycompany/study/ch07/third/Calculator.java
@@ -0,0 +1,10 @@
+package com.mycompany.study.ch07.third;
+
+public class Calculator {
+
+	public double areaCircle(double r) {
+		System.out.println("Carculator 객체의 areaCircle실행");
+		
+		return 3.14159 * r * r;
+	}
+}
