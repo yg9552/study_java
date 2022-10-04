7e698cf8ccd87f42035ca2a163b18afd6a4a4043
yg9552 <yg4583@naver.com>
2022-10-04 16:06:35 +0900

    .

--- src/com/mycompany/study/ch12/first/BeepTask.java
+++ src/com/mycompany/study/ch12/first/BeepTask.java
@@ -1,5 +1,14 @@
 package com.mycompany.study.ch12.first;
 
-public class BeepTask {
+import java.awt.Toolkit;
+
+public class BeepTask implements Runnable{
+	public void run() {		
+		Toolkit toolkit = Toolkit.getDefaultToolkit();	
+		for(int i=0; i<5; i++) {		
+			toolkit.beep();
+			try { Thread.sleep(500); } catch(Exception e) {}
+		}
+	}
 
 }
