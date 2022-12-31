d557f88a9d642346d1043a42455506c213bf29af
yg9552 <yg4583@naver.com>
2022-12-31 10:58:16 +0900

    ...

---
+++ src/com/mycompany/study/ch07/fifth/Score.java
@@ -0,0 +1,10 @@
+package com.mycompany.study.ch07.fifth;
+
+public class Score implements Comparable<Score> {
+	 private int point;
+	     public Score(int point) { this.point = point; }
+	   //  public int compareTo(Object o){/*more code here*/}
+	  //   public int compareTo(Score other){/*more code here*/}
+	  //   public int compare(Score s1,Score s2){/*more code here*/}
+	  //   public int compare(Object o1,Object o2){/*more code here*/}
+}
---
+++ src/com/mycompany/study/ch07/fifth/Triangle.java
@@ -0,0 +1,14 @@
+package com.mycompany.study.ch07.fifth;
+
+import com.mycompany.study.ch07.fifth.Line.Point;
+
+public class Triangle {
+	 public Triangle() {
+		        // insert code here
+	 Point p = Line.getPoint();
+	//	 Line.Point p = Line.getPoint();
+	//	 Point p = (new Line()).getPoint();
+	//	 Line.Point p = (new Line()).getPoint();
+		 
+		   }
+}
